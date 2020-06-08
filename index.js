d8e09a9ecb02997a0a900faac6e32d829e6ed6fc
Ryan Hafner <r.hafner20@gmail.com>
2020-06-08 18:36:53 -0400

    create primitives, relegate creation of objects to update function, enabled shadowMap in component

--- index.js
+++ index.js
@@ -1,76 +1,178 @@
+'use strict';
+
 AFRAME.registerComponent('shadow-light', {
     schema: {
         type: { type: 'string', default: 'directional', oneOf: [ 'directional', 'spot' ] },
         intensity: { type: 'float', default: 0.5, min: 0 },
         color: { type: 'color', default: 'white' },
-        angle: { type: 'float', default: -Math.PI/5 },
+        target: { type: 'vec3', default: { x: 0, y: 0, z: 0 } },
         near: { type: 'float', default: 1 },
         far: { type: 'float', default: 5 },
         tlrb: { type: 'vec4', default: { x: 5, y: -5, z: 5, w: -5 } },
-        mapSize: { type: 'vec2', default: { x: 1024, y: 1024 } },
-        bias: { type: 'float', default: 0 },
+        mapsize: { type: 'vec2', default: { x: 1024, y: 1024 } },
+        bias: { type: 'float', default: -0.000222 },
         helper: { type: 'boolean', default: false }
     },
     init() {
-        const { el, data: { type, color, intensity, angle, bias, mapSize, near, far, tlrb, helper } } = this;
-        const { object3D: scene } = el.sceneEl;
-
+        this.el.sceneEl.renderer.shadowMap.enabled = true;
+        this.el.shadowLight = null;
+    },
+    update(oldData) {
+        const diff = AFRAME.utils.diff(oldData, this.data);
+        
+        if (diff.hasOwnProperty('type')) {
+            this.remove();
+            this._createLight();
+            return;
+        }
+        
+        for (var key of Object.keys(diff)) {
+            switch(key) {
+                case 'intensity':
+                case 'color':
+                    this.light[key] = this.data[key];
+                    break;
+                case 'target':
+                    const { x, y, z } = this.data.target;
+                    this.light.target.position.set(x, y, z);
+                    break;
+                case 'near':
+                case 'far':
+                    this.light.shadow.camera[key] = this.data[key];
+                    break;
+                case 'tlrb':
+                    const [ t, l, r, b ] = Object.values(this.data.tlrb).map(parseFloat);
+                    this.light.shadow.camera.top = top;
+                    this.light.shadow.camera.left = left;
+                    this.light.shadow.camera.right = right;
+                    this.light.shadow.camera.bottom = bottom;
+                    break;
+                case 'mapsize':
+                    const { x: w, y: h } = this.data.mapsize;
+                    this.light.shadow.mapSize.width = w;
+                    this.light.shadow.mapSize.height = h;
+                    break;
+                case 'bias':
+                    this.light.shadow.bias = this.data.bias;
+                    break;
+                case 'helper':
+                    const { object3D: scene } = this.el.sceneEl;
+                    if (this.data.helper && !this.helper) {
+                        this.helper = new THREE.CameraHelper(this.light.shadow.camera);
+                        scene.add(this.helper);
+                    }
+                    else if (!this.data.helper && this.helper) {
+                        scene.remove(this.helper);
+                        this.helper = null;
+                    }
+            }
+        }
+    },
+    remove() {
+        const { object3D: scene } = this.el.sceneEl;
+        if (this.light) {
+            scene.remove(this.light.target);
+            this.el.removeObject3D('light');
+        }
+        if (this.helper) {
+            scene.remove(this.helper);
+            this.helper = null;
+        }
+        this.light = null;
+    },
+    _createLight() {
+        const { type, color, intensity, target, bias, mapsize, near, far, tlrb, helper } = this.data;
+        const { object3D: scene } = this.el.sceneEl;
+        
         const light = type == 'directional' ? 
             new THREE.DirectionalLight(color, intensity):
             new THREE.SpotLight(color, intensity);
-        light.angle = angle;
+        
+        scene.add(light.target);
+        light.target.position.set(target.x, target.y, target.z);
         
         light.shadow.bias = bias;
-        light.shadow.mapSize.width = mapSize.x;
-        light.shadow.mapSize.height = mapSize.y;
+        light.shadow.mapSize.width = mapsize.x;
+        light.shadow.mapSize.height = mapsize.y;
 
         light.shadow.camera.near = near;
         light.shadow.camera.far = far;
-        const [ top, left, right, bottom ] = Object.values(tlrb).map(function(a) { return parseFloat(a) });
+        const [ top, left, right, bottom ] = Object.values(tlrb).map(parseFloat);
         light.shadow.camera.top = top;
         light.shadow.camera.left = left;
         light.shadow.camera.right = right;
         light.shadow.camera.bottom = bottom;
 
         light.castShadow = true;
-        el.setObject3D('light', light);
+        this.el.setObject3D('light', light);
 
         if (helper) {
             this.helper = new THREE.CameraHelper(light.shadow.camera);
             scene.add(this.helper);
         }
         this.light = light;
-
-        // el.sceneEl.renderer.shadowMap.enabled = true;
+        this.el.shadowLight = light;
     }
 });
 
 AFRAME.registerComponent('shadow-plane', {
     schema: {
-        dimensions: { type: 'vec2', default: { x: 100, y: 100 } },
+        dimensions: { type: 'vec2', default: { x: 10, y: 10 } },
         opacity: { type: 'float', default: 0.2, min: 0, max: 1 }
     },
     init() {
-        const { el, data: { dimensions, opacity } } = this;
-        const planeGeometry = new THREE.PlaneBufferGeometry(dimensions.x, dimensions.y);
-        planeGeometry.rotateX(-Math.PI/2);
-        const planeMaterial = new THREE.ShadowMaterial({ opacity });
-
-        const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-        plane.position.y = 0;
-        plane.receiveShadow = true;
-        el.setObject3D('mesh', plane);
-
-        // el.sceneEl.renderer.shadowMap.enabled = true;
+        this.el.sceneEl.renderer.shadowMap.enabled = true;
+    },
+    update(oldData) {
+        const diff = AFRAME.utils.diff(oldData, this.data);
+        if (diff.dimensions) {
+            this.remove();
+            this._createPlane();
+        }
+        else this.material.opacity = this.data.opacity
+    },
+    remove() {
+        if (!this.plane) return;
+        this.el.removeObject3D('mesh');
+        this.plane = null;
+    },
+    _createPlane() {
+        const { dimensions: { x: w, y: h }, opacity } = this.data;
+        
+        const geo = new THREE.PlaneBufferGeometry(w, h);
+        geo.rotateX(-Math.PI/2);
+        this.material = new THREE.ShadowMaterial({ opacity });
+        
+        this.plane = new THREE.Mesh(geo, this.material);
+        this.plane.receiveShadow = true;
+        this.el.setObject3D('mesh', this.plane);
     }
 });
 
-window.addEventListener('load', function() {
-    const onxrloaded = function() {
-        const [ SCENE ] = AFRAME.scenes;
-        if (!SCENE) return setTimeout(onxrloaded, 100);
-        SCENE.renderer.shadowMap.enabled = true;
+AFRAME.registerPrimitive('a-shadow-light', {
+    defaultComponents: {
+        'shadow-light': {}
+    },
+    mappings: {
+        type: 'shadow-light.type',
+        intensity: 'shadow-light.intensity',
+        color: 'shadow-light.color',
+        target: 'shadow-light.target',
+        near: 'shadow-light.near',
+        far: 'shadow-light.far',
+        tlrb: 'shadow-light.tlrb',
+        mapsize: 'shadow-light.mapsize',
+        bias: 'shadow-light.bias',
+        helper: 'shadow-light.helper'
     }
-    if (window.XR8) onxrloaded();
-    else window.addEventListener('xrloaded', onxrloaded);
+});
+
+AFRAME.registerPrimitive('a-shadow-plane', {
+    defaultComponents: {
+        'shadow-plane': {}
+    },
+    mappings: {
+        dimensions: 'shadow-plane.dimensions',
+        opacity: 'shadow-plane.opacity'
+    }
 });
